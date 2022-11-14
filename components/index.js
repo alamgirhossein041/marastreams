6d6f6134fce6f0851364930b68a8f40f4df94d80
krypt007 <steve@calstec.com>
2022-11-14 15:29:12 +0300

    import error fixes

--- components/index.js
+++ components/index.js
@@ -13,8 +13,8 @@ export { default as Button } from './Button';
 
 // export { default as Sidebar } from './Sidebar';
 // // eslint-disable-next-line import/no-cycle
-// export { default as Cart } from './Cart';
-// export { default as Chat } from './Chat';
+export { default as Cart } from './Cart';
+export { default as Chat } from './Chat';
 export { default as Notification } from './Notification';
 export { default as UserProfile } from './UserProfile';
 // export { default as SparkLine } from './Charts/SparkLine';
